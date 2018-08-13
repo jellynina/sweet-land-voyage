95ddad7a21aa8d823ebddfe45b8e57d34c4c46d7
jellynina <jellynina@gmail.com>
2018-08-13 22:00:16 +0800

    remove package-lock

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*== Sphero Connection ==*/
 var Sphero = require("./node_modules/sphero/lib/sphero.js");
 var theBall = new Sphero('af1f81cb842f4308a56657398413d334');
 theBall.connect(function () {
